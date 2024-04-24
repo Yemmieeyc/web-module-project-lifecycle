523b4f84c7c3336d0d3f3d8b0848cb6519faefaf
Yemmieeyc <adewole_aina@yahoo.com>
2024-04-24 11:03:11 -0700

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,6 +8,7 @@ export default class App extends React.Component {
     todos: [],
     error:'',
     todoNameInput:'',
+    displayCompleteds: true,
   }
   onTodoNameChange = evt => {
     const {value} = evt.target
@@ -56,6 +57,9 @@ toogleCompleted = id => () => {
   componentDidMount(){
     this.fetchAllTodos()
   }
+  toogleDisplayCompleteds = () => {
+    this.setState({...this.state, displayCompleteds: !this.state.displayCompleteds})
+  }
  
   render() {
     return (
@@ -63,17 +67,20 @@ toogleCompleted = id => () => {
         <div id='error'>{this.state.error}</div>
         <div>
         <h2>Todos:</h2>
-        { this.state.todos.map(td => {
-          return <div onClick={this.toogleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✔️ ' : ''}</div>
-        })
-          
+       {
+          this.state.todos.reduce((acc, td) => {
+            if (this.state.displayCompleteds || !td.completed) return acc.concat(<div onClick={this.toogleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✔️ ' : ''}</div>)
+            return acc
+          }, [])
+          //return <div onClick={this.toogleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✔️ ' : ''}</div>
+                  
         }
         </div>
       <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
         <input value={this.state.todoNameInput} onChange={this.onTodoNameChange} type='text' placeholder='Type todo'></input>
         <input type='submit'></input>
-        <button>Clear Completed</button>
-      </form>
+        </form>
+        <button onClick={this.toogleDisplayCompleteds}>{this.state.displayCompleteds ? 'Hide' : 'Show'} Completed</button>
        </div>
     )
   }
